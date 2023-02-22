d8c9cfeeb209d3b1e92e0782dedc4c92637166f1
dimanakov <nasdaq80@mail.ru>
2023-02-22 18:25:22 +0300

    fix: reformat JS, edit CSS button_focus name

--- scripts/index.js
+++ scripts/index.js
@@ -4,31 +4,29 @@ let profileForm = document.querySelector('.profile-form');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
 // имя и профессия в profile-form
-let formName = document.querySelector('.profile-form__item_el_name')
+let formName = document.querySelector('.profile-form__item_el_name');
 let formProfession = document.querySelector('.profile-form__item_el_profession');
-
 // кнопки
 let profileEditButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-
 // открыть popup при нажатии кнопки edit
 profileEditButton.addEventListener('click', function () {
-  popup.classList.toggle('popup_opened');
+  popup.classList.add('popup_opened');
   // перенести данные со страницы в форму
   formName.value = profileName.textContent; // all <input> have property .value
-  formProfession.value = profileProfession.textContent}); // to read entered text
-
+  formProfession.value = profileProfession.textContent; // to read entered text
+});
 // закрыть popup без сохранения
-function closeButtonEvent(){
-  popup.classList.toggle('popup_opened')
-  }
-closeButton.addEventListener('click', closeButtonEvent);
-
+function closePopupEvent() {
+  popup.classList.remove('popup_opened')
+}
 // сохранить данные из формы на странице
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = formName.value;
   profileProfession.textContent = formProfession.value;
-  closeButtonEvent();
-  }
+  closePopupEvent();
+}
+// действия на странице
+closeButton.addEventListener('click', closePopupEvent);
 profileForm.addEventListener('submit', handleFormSubmit);
