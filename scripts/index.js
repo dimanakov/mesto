50ece4005990cb2acfc10112eead6c6bc9d9a20c
E-spacnvrnd <nasdaq80@mail.ru>
2023-03-03 19:11:03 +0300

    fix: file names close & cursor, remov extra images

--- scripts/index.js
+++ scripts/index.js
@@ -16,8 +16,8 @@ const cards = [
     link: 'https://images.unsplash.com/photo-1535557142533-b5e1cc6e2a5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2129&q=80'
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Минеральные воды',
+    link: '../images/elements__min-vodi.jpg'
   },
   {
     name: 'Байкал',
